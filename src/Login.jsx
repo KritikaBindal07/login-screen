6e3bc3989836b21cdcb552e2969732d8ede19e73
Kritika Bindal <kirtibindalkb770@gmail.com>
2023-07-17 15:58:35 +0530

    login

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { useState } from "react";
 import axios from "axios";
 
-import {Routes, Route, useNavigate} from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 
 
 
